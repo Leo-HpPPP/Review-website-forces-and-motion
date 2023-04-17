7c5fe6857227bc5fbc83051b96ac6ed01e0b28f9
Leo-HpPPP <130573779+Leo-HpPPP@users.noreply.github.com>
2023-04-17 13:19:07 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,15 @@
+    function scrollcheck(){
+  var title = document.getElementById("main").offsetTop;
+    const boxes = document.querySelectorAll('.sections');
+    var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset+140 : (document.documentElement || document.body.parentNode || document.body).scrollTop+140;
+    if(scrollTop > title){
+        document.getElementById("main").style.background = "#63b4cf";
+    
 
+    }else {
+      document.getElementById("main").style.background = 'lightblue';
+      document.getElementById("main").style.fontSize = "15px";
+    }
+    setTimeout(scrollcheck, 10);
+}
+scrollcheck();
